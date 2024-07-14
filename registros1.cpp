aa8271f2cc88015e8ae54e68b73ff9fb24d3bea0
atellezm1 <atellezm@unjbg.edu.pe>
2024-07-14 08:19:27 -0500

    En registros1.cpp se implementa un for para obtener los datos del empleado con menor sueldo para posteriormente imprimir sus datos en la pantalla.

--- registros1.cpp
+++ registros1.cpp
@@ -12,9 +12,11 @@ struct mpdatatech{
 
 int main (){
     mpdatatech empleado[10];
-    int n,may,men,a;
+    int n,may,men,a,b;
+    b=0;
     a=0;
     may=0;
+    men=999999;
     cout<<"introdusca la cantidad de empleados: "<<endl;
     cin>>n;
     for(int i=1;i<=n;i++){
@@ -32,7 +34,16 @@ int main (){
         }
     }
     cout<<"El empleado con mayor sueldo es: "<<empleado[a].nombres<<endl;
-    cout<<"Con un sueldo de: "<<empleado[a].sueldo;
+    cout<<"Con un sueldo de: "<<empleado[a].sueldo<<endl;
+
+    for(int i=1;i<=n;i++){
+        if(men>empleado[i].sueldo){
+            men=empleado[i].sueldo;
+            b=i;
+        }
+    }
+    cout<<"El empleado con menor sueldo es: "<<empleado[b].nombres<<endl;
+    cout<<"Con un sueldo de: "<<empleado[b].sueldo<<endl;
 
     return 0;
 }
