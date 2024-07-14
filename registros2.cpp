4fcf02f3f71991a875fba2334a02d53eb2af5c26
atellezm1 <atellezm@unjbg.edu.pe>
2024-07-14 13:39:32 -0500

    Implementando en registros2 la capacidad de imprimir el promedio de las edades y tambien los datos almacenados en el registro

--- registros2.cpp
+++ registros2.cpp
@@ -1,5 +1,6 @@
 //Alexander Jesús Téllez Mesa
 #include<iostream>
+#include<string>
 using namespace std;
 
 struct persona{
@@ -9,25 +10,42 @@ struct persona{
 
 };
 
-
 int main (){
     persona persn[50];
-    int n,may,men,a,b;
-    b=0;
+    int n,may,a,p;
     a=0;
     may=0;
-    men=999999;
     cout<<"introdusca la cantidad de personas: "<<endl;
     cin>>n;
+    cin.ignore(100,'\n');
     for(int i=1;i<=n;i++){
         cout<<"ingre los nombres de la persona "<<i<<":"<<endl;
-        cin>>persn[i].nombres;
+        getline(cin,persn[i].nombres);
         cout<<"ingrese el DNI de la persona "<<i<<":"<<endl;
         cin>>persn[i].DNI;
         cout<<"ingrese la edad de la persona "<<i<<":"<<endl;
         cin>>persn[i].edad;
+        cin.ignore(1000,'\n');
+    }
+    for(int i=1; i<=n;i++){
+        if(persn[i].edad>50){
+            may=may+1;
+        }
+    }
+    cout<<"la cantidad de personas mayores de 50 años son: "<<may<<endl;
+     
+     for(int i=1;i<=n;i++){
+        a=persn[i].edad+a;
+     }
+    p=a/n;
+    cout<<"el promedio de las edades de todas las personas es:"<<p<<endl;
+    cout<<"LOS DATOS INGRESADOS DE TODAS LAS PERSONAS SON: "<<endl;
+    for(int i=1; i<=n;i++){
+        cout<<"nombres: "<<persn[i].nombres<<endl;
+        cout<<"numero de DNI: "<<persn[i].DNI<<endl;
+        cout<<"edad de la persona: "<<persn[i].edad<<endl;
     }
 
-
+	
     return 0;
 }
